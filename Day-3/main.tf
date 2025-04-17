47c233d1b630bf3d5c443e36748d58143bf8e0fd
Akshata Teli <85943049+Akshata0211@users.noreply.github.com>
2025-04-17 11:51:03 +0000

    Added subnet

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-1"
 }
 
 module "ec2_instance" {
   source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
+  ami_value = "ami-084568db4383264d4" # replace this
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+  subnet_id_value = "subnet-05bc8e29e473b412d". # replace this
 }
